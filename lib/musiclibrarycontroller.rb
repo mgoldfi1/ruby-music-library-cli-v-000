94ef0d3dd9ba6d6e20d49dd8000dc04bc47e57e4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-03 13:31:01 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,7 +7,7 @@ def initialize(path = "./db/mp3s")
 end
 
 def call
-loop do 
+loop do
 puts "Welcome to your music library!"
 puts "To list all of your songs, enter 'list songs'."
 puts "To list all of the artists in your library, enter 'list artists'."
