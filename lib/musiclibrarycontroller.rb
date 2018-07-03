6611d77a2fc837ba5b319b1091dfe1aac659f451
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-03 12:46:39 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -2,8 +2,9 @@ require 'pry'
 class MusicLibraryController
 
 def intialize(path = "./db/mp3s/")
-  MusicImporter.new(path = "./db/mp3s/")
   binding.pry
+  MusicImporter.new(path = "./db/mp3s/")
+
 end
 
 
