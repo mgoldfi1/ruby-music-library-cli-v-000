d533e04bb7e0f682d3447ba2e127a88fb64ad2d8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-03 14:04:49 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -43,7 +43,7 @@ def list_genres
 def list_songs_by_artist
 puts "Please enter the name of an artist:"
 choice = gets.strip
-Arist.all.each do |x|
+Artist.all.each do |x|
   if x.name == choice
     x.songs.each_with_index do |song,index|
     puts "#{index+1}. #{song.name} - #{song.genre.name}"
